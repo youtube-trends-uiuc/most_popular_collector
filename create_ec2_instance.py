831c1b43f4161ef20ae904bda9685bf866d7d491
alexgonca <alexandre.reportagem@gmail.com>
2025-08-21 13:42:38 -0500

    create virtual environment

--- create_ec2_instance.py
+++ create_ec2_instance.py
@@ -1,71 +1,46 @@
 import boto3
-from urllib.parse import urlparse
-import json
-import uuid
 
 
 INIT_SCRIPT = """#!/bin/bash
 cd /home/ubuntu
 su ubuntu -c 'mkdir .aws'
-su ubuntu -c 'printf "[default]\\nregion={region}" > /home/ubuntu/.aws/config'
-su ubuntu -c 'wget {init_script} -O {new_name}'
-su ubuntu -c 'chmod +x {new_name}'
-su ubuntu -c "echo '/home/ubuntu/{new_name} {s3_config}' > call.txt"
-su ubuntu -c 'sudo apt-get update'
+su ubuntu -c 'printf "[default]\\nregion=us-west-2" > /home/ubuntu/.aws/config'
+su ubuntu -c 'wget https://raw.githubusercontent.com/youtube-trends-uiuc/recommendations_collector/refs/heads/main/init_script.sh'
+su ubuntu -c 'chmod +x /home/ubuntu/init_script.sh'
+su ubuntu -c 'sudo apt-get update -y'
 su ubuntu -c 'sudo apt-get install -y screen'
-su ubuntu -c "screen -dmS internet_scholar sh -c '/home/ubuntu/{new_name} {s3_config} 2>&1 | tee output.txt; exec bash'"
+su ubuntu -c "screen -dmS youtube_trends sh -c '/home/ubuntu/init_script.sh 2>&1 | tee output.txt; exec bash'"
 """
 
-aws_config = {
-    "s3-admin": "internet-scholar-admin",
-    "s3-data": "internet-scholar",
-    "athena-admin": "internet_scholar_admin",
-    "athena-data": "internet_scholar",
-    "default_region": "us-west-2",
-    "key_name": "webscholar",
-    "security_group": "launch-wizard-1",
-    "iam": "ec2_internetscholar",
-    "instance_type": "t3a.nano",
-    "volume_size": 15,
-    "name": "youtube-video-snippet",
-    "init_script": "https://raw.githubusercontent.com/internet-scholar/youtube_video_snippet/master/init_script.sh",
-    "ami": "ami-020d545e05dd191a9"
-}
-
-
 def lambda_handler(event, context):
-    url_object = urlparse(event['s3_config'], allow_fragments=False)
-    s3 = boto3.resource('s3')
-    content_object = s3.Object(url_object.netloc, url_object.path.lstrip('/'))
-    file_content = content_object.get()['Body'].read().decode('utf-8')
-    config = json.loads(file_content)
-
-    new_name = f"{uuid.uuid4()}.sh"
-    init_script = INIT_SCRIPT.format(init_script=config['aws']['init_script'],
-                                     region=config['aws']['default_region'],
-                                     s3_config=event['s3_config'],
-                                     new_name=new_name)
-
     ec2 = boto3.resource('ec2')
     ec2.create_instances(
-        ImageId=config['aws']['ami'],
-        InstanceType=config['aws']['instance_type'],
+        ImageId='ami-0836fd4a4a0b4f6ec',
+        InstanceType='t4g.nano',
         MinCount=1,
         MaxCount=1,
-        KeyName=config['aws']['key_name'],
+        KeyName='Dec9-2019-key',
         InstanceInitiatedShutdownBehavior='terminate',
-        UserData=init_script,
-        SecurityGroupIds=[config['aws']['security_group']],
+        UserData=INIT_SCRIPT,
+        SecurityGroupIds=['sg-0b6d936cbbaab78f2',],
         BlockDeviceMappings=[
             {
                 'DeviceName': '/dev/sda1',
                 'Ebs': {
                     'DeleteOnTermination': True,
-                    'VolumeSize': config['aws']['volume_size']
+                    'VolumeSize': 15
                 }
             },
         ],
         TagSpecifications=[{'ResourceType': 'instance',
-                            'Tags': [{"Key": "Name", "Value": config['aws']['name']}]}],
-        IamInstanceProfile={'Name': config['aws']['iam']}
+                            'Tags': [{"Key": "Name", "Value": 'youtube_trends'}]}],
+        IamInstanceProfile={'Name': 'youtube_trends'}
     )
+    return {
+        "statusCode": 200,
+        "message": "Instance created!"
+    }
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
--- init_script.sh
+++ init_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sudo timedatectl set-timezone UTC
-sudo apt-get update -y
 sudo apt-get install -y python3-pip default-jre
 cd /home/ubuntu
 wget https://repo1.maven.org/maven2/org/apache/orc/orc-tools/2.2.0/orc-tools-2.2.0-uber.jar
 wget https://raw.githubusercontent.com/youtube-trends-uiuc/recommendations_collector/refs/heads/main/collect_recommendations.py
 wget https://raw.githubusercontent.com/youtube-trends-uiuc/recommendations_collector/refs/heads/main/requirements.txt
 wget https://raw.githubusercontent.com/youtube-trends-uiuc/recommendations_collector/refs/heads/main/upload_recommendations.py
+python3 -m venv ./venv
+source ./venv/bin/activate
 pip3 install --trusted-host pypi.python.org -r ./requirements.txt
 python3 ./collect_recommendations.py && \
 java -jar ./orc-tools-2.2.0-uber.jar convert recommendations.json -s 'struct<kind:string,etag:string,id:string,snippet:struct<publishedAt:timestamp,title:string,description:string,channelId:string,channelTitle:string,categoryId:string,tags:array<string>,liveBroadcastContent:string,defaultLanguage:string,defaultAudioLanguage:string,localized:struct<title:string,description:string>,thumbnails:struct<default:struct<url:string,width:int,height:int>,medium:struct<url:string,width:int,height:int>,high:struct<url:string,width:int,height:int>,standard:struct<url:string,width:int,height:int>,maxres:struct<url:string,width:int,height:int>>>,statistics:struct<viewCount:bigint,likeCount:bigint,dislikeCount:bigint,favoriteCount:bigint,commentCount:bigint>,metadata:struct<region_code:string,retrieved_at:timestamp,rank:int>>' -o json.orc -t "yyyy-MM-dd HH:mm:ss.n" 2>&1 | tee ./orc_output.log && \
