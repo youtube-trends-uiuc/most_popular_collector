2c8803040852d0cd8556664f61e540ee9e395b04
alexgonca <alexandre.reportagem@gmail.com>
2025-08-21 15:50:20 -0500

    tweak upload_most_popular.py

--- upload_most_popular.py
+++ upload_most_popular.py
@@ -7,9 +7,9 @@ def upload_most_popular():
     # TODO: Maybe make sure that period is in [0, 6, 12, 18]
     period = datetime.datetime.now(datetime.UTC).strftime("%H")
     s3.Bucket('youtube-trends-uiuc-json').upload_file("./most_popular.json.bz2",
-                                                      f"creation_date={creation_date}/period={period}/recommendations.json.bz2")
+                                                      f"creation_date={creation_date}/period={period}/most_popular.json.bz2")
     s3.Bucket('youtube-trends-uiuc').upload_file("./most_popular.orc",
-                                                 f"creation_date={creation_date}/period={period}/json.orc")
+                                                 f"creation_date={creation_date}/period={period}/most_popular.orc")
 
 
 if __name__ == '__main__':
