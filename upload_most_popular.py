c2a5a781be24c49fd4dc81a7bdf0b4a9aa21e57d
alexgonca <alexandre.reportagem@gmail.com>
2025-08-24 09:54:47 -0500

    change destination directory S3 backup files

--- upload_most_popular.py
+++ upload_most_popular.py
@@ -6,8 +6,8 @@ def upload_most_popular():
     creation_date = datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d")
     # TODO: Maybe make sure that period is in [0, 6, 12, 18]
     period = datetime.datetime.now(datetime.UTC).strftime("%H")
-    s3.Bucket('youtube-trends-uiuc-json').upload_file("./most_popular.json.bz2",
-                                                      f"creation_date={creation_date}/period={period}/most_popular.json.bz2")
+    s3.Bucket('youtube-trends-uiuc-backup').upload_file("./most_popular.json.bz2",
+                                                      f"json/creation_date={creation_date}/period={period}/most_popular.json.bz2")
     s3.Bucket('youtube-trends-uiuc').upload_file("./most_popular.orc",
                                                  f"creation_date={creation_date}/period={period}/most_popular.orc")
 
